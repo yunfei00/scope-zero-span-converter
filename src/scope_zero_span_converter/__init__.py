ddfc76971f5396acd6003c3b9b8290d14d1a113d
yunfei00 <goodman_yunfei@163.com>
2026-09-03 19:17:31 +0800

    chore: 同步包版本到 0.2.0

--- src/scope_zero_span_converter/__init__.py
+++ src/scope_zero_span_converter/__init__.py
@@ -2,7 +2,7 @@
 
 from .plotting import configure_matplotlib_chinese
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 # 在 GUI / CLI 创建任何 Figure 之前统一配置中文字体。
 # Windows 客户机优先使用 Microsoft YaHei；没有候选字体时安全回退。
