6621980a2671539326838b3ead1f98fd8906f12e
yunfei00 <goodman_yunfei@163.com>
2026-09-07 17:25:45 +0800

    test: 兼容main开发版本与tag发布版本校验

--- tests/test_versioning.py
+++ tests/test_versioning.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 import re
 
 import scope_zero_span_converter as package
@@ -14,5 +15,9 @@ def test_package_version_comes_from_single_version_module():
     assert PEP440_SIMPLE.match(package.__version__)
 
 
-def test_main_branch_is_on_next_commercialization_development_line():
-    assert package.__version__ == "0.8.0.dev0"
+def test_version_matches_current_build_context():
+    ref_name = os.environ.get("GITHUB_REF_NAME", "")
+    if ref_name.startswith("v"):
+        assert package.__version__ == ref_name[1:]
+    else:
+        assert package.__version__ == "0.8.0.dev0"
