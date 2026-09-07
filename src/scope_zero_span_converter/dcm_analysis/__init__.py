9014d1cb3cf3619f0074def60b0f3c47e1121465
yunfei00 <goodman_yunfei@163.com>
2026-09-07 17:40:35 +0800

    refactor: 统一导出DCM分析正式模块

--- src/scope_zero_span_converter/dcm_analysis/__init__.py
+++ src/scope_zero_span_converter/dcm_analysis/__init__.py
@@ -5,6 +5,28 @@ versioned widget modules remain temporarily as compatibility shims while the
 GUI is migrated in stages.
 """
 
+from .axis import (
+    apply_fixed_xy_axis,
+    apply_fixed_y_axis,
+    automatic_bounds,
+    fixed_ticks,
+    major_tick_step,
+    nice_step,
+)
 from .spectrum import DcmSpectrum, compute_dcm_spectrum
+from .zoom import ZoomBounds, ZoomState, ZoomTarget, normalized_bounds
 
-__all__ = ["DcmSpectrum", "compute_dcm_spectrum"]
+__all__ = [
+    "DcmSpectrum",
+    "compute_dcm_spectrum",
+    "fixed_ticks",
+    "nice_step",
+    "automatic_bounds",
+    "major_tick_step",
+    "apply_fixed_y_axis",
+    "apply_fixed_xy_axis",
+    "ZoomTarget",
+    "ZoomBounds",
+    "ZoomState",
+    "normalized_bounds",
+]
